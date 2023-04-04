87afbef8ae4e002ca1bdc5a8ee4f654046492f34
81866-MatiasGregorat <19mati3@gmail.com>
2023-04-04 16:08:12 -0300

    Commit Inicial del proyecto
    Esto es una descripcion

---
+++ src/main/java/grupox/personas/Cliente.java
@@ -0,0 +1,4 @@
+package grupox.personas;
+
+public class Cliente {
+}
---
+++ src/main/java/grupox/utilidades/App.java
@@ -0,0 +1,16 @@
+package grupox.utilidades;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hola Mundo" );
+
+        System.out.println("Otro mensaje, otro cambio");
+    }
+}
